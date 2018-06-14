7d9d34ac352e3a77a893361ba158e24125bdd98b
LeoChan <236338364@qq.com>
2018-06-14 23:18:35 +0800

    vuex

--- src/components/page/Blog.vue
+++ src/components/page/Blog.vue
@@ -1,61 +1,49 @@
 <template>
-	<div>
-		<div class="father-area">
-			id: <input type="text" v-model="getId">
-			title: <input type="text" v-model="getTitle">
-			tag: <input type="text" v-model="getTag">
-
-			<!-- id: <input type="text" v-model="currentArticle.id">
-			title: <input type="text" v-model="currentArticle.title">
-			tag: <input type="text" v-model="currentArticle.tag"> -->
-		</div>
-		<div class="child-area">
-			<Leftnav></Leftnav>
-		</div>
-	</div>
+  <div>
+    <div class="father-area">
+      id: <input type="text" v-model="currentArticle.id">
+      title: <input type="text" v-model="currentArticle.title">
+      tag: <input type="text" v-model="currentArticle.tag">
+      count: <input type="text" v-model="count">
+    </div>
+    <div class="child-area">
+      <Leftnav></Leftnav>
+    </div>
+  </div>
 </template>
 
 <script>
-	import Leftnav from '../common/LeftNavItem'
-	import store from '../../store/demo'
-	import { mapState } from 'vuex';
-	export default {
-		name: 'blog',
-		data() {
-			return {
-				message: 'father message'
-			}
-		},
-		components: {
-			Leftnav
-		},
-		methods: {
+  import Leftnav from '../common/LeftNavItem'
+  import { mapState } from 'vuex';
+  export default {
+    name: 'blog',
+    data() {
+      return {
+        message: 'father message'
+      }
+    },
+    components: {
+      Leftnav
+    },
+    methods: {
 
-		},
-		computed: {
-			getId() {
-				console.log(this.currentArticle);
-				return store.state.currentArticle.id
-			},
-			getTitle() {
-				return store.state.currentArticle.title
-			},
-			getTag() {
-				return store.state.currentArticle.tag
-			},
-			...mapState(['currentArticle'])
-		}
-	}
+    },
+    // computed: mapState(['currentArticle', 'count'])
+    computed: mapState({
+      currentArticle: 'currentArticle',
+      count: 'count',
+    })
+  }
 </script>
 
 <style scoped>
-	.father-area {
-		background-color: aqua;
-		padding: 10px;
-	}
-	.child-area {
-		background-color: bisque;
-		padding: 10px;
-	}
+  .father-area {
+    background-color: aqua;
+    padding: 10px;
+  }
+  .child-area {
+    background-color: bisque;
+    padding: 10px;
+  }
 </style>
 
--- src/store/demo.js
+++ src/store/demo.js
@@ -4,17 +4,22 @@ import Vuex from 'vuex'
 Vue.use(Vuex);
 
 const state = {
-  currentArticle: {id: '1', title: '学习笔记', tag: 'vue'}   // 当前文章状态, 名字, 标签
+  currentArticle: {id: '1', title: '学习笔记', tag: 'vue'},   // 当前文章状态, 名字, 标签
+  count: 1
 }
 
 const types = {
-  CURRENT_ARTICLE: 'CURRENT_ARTICLE'
+  CURRENT_ARTICLE: 'CURRENT_ARTICLE',
+  COUNT: 'COUNT'
 }
 
 const actions = {
   currentArticle({commit}, obj) {
     commit(types.CURRENT_ARTICLE, obj);
   },
+  countIncrement({commit}, n) {
+    commit(types.CURRENT_ARTICLE, n);
+  }
 }
 
 const mutations = {
@@ -23,6 +28,9 @@ const mutations = {
     obj.title == undefined ? false : state.currentArticle.title = obj.title;
     obj.tag == undefined ? false : state.currentArticle.tag = obj.tag;
     obj.catalog == undefined ? false : state.currentArticle.catalog = obj.catalog;
+  },
+  [types.COUNT](state, n = 1) {
+    state.count += n;
   }
 }
 
