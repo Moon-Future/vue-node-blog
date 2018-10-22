1aab917eea31568b6c61efe35a4d657b2cc44a96
LeoChan <236338364@qq.com>
2018-10-22 21:55:39 +0800

    home

---
+++ src/components/front/TopHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header">
+    <div class="background">
+      <img src="https://source.unsplash.com/random/1366x520" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'topHeader',
+  }
+</script>
+
+<style lang="scss" scoped>
+  .background {
+    img {
+      width: 100%;
+    }
+  }
+</style>
