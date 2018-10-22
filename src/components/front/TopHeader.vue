4bc8280ebb10abf079fd5ad99db33b818c69dfa9
Leo Chan <236338364@qq.com>
2018-10-23 00:19:24 +0800

    header

--- src/components/front/TopHeader.vue
+++ src/components/front/TopHeader.vue
@@ -2,6 +2,21 @@
   <div class="header">
     <div class="background">
       <img src="https://source.unsplash.com/random/1366x520" alt="">
+      <div class="txt">
+        <p class="first-line">心有猛虎</p>
+        <p class="second-line">细嗅蔷薇</p>
+      </div>
+    </div>
+    <div class="nav">
+      <ul>
+        <li>主页</li>
+        <li>目录</li>
+        <li class="avatar">
+          <img src="../../assets/avatar.jpg" alt="">
+        </li>
+        <li>其他</li>
+        <li>关于</li>
+      </ul>
     </div>
   </div>
 </template>
@@ -9,13 +24,62 @@
 <script>
   export default {
     name: 'topHeader',
+    data() {
+      return {
+
+      }
+    }
   }
 </script>
 
 <style lang="scss" scoped>
+  @import '@/common/css/variable.scss';
   .background {
+    position: fixed;
+    top: 0;
+    z-index: -1;
+    width: 100%;
     img {
       width: 100%;
+      height: 520px;
+    }
+    .txt {
+      position: absolute;
+      top: 50%;
+      width: 100%;
+      color: $color-white;
+      font-size: 32px;
+      font-family: KaiTi, SimSun;
+      .first-line {
+        text-indent: -50px;
+      }
+      .second-line {
+        text-indent: 50px;
+      }
+    }
+  }
+  .nav {
+    position: fixed;
+    top: 0;
+    width: 100%;
+    height: 50px;
+    background: $color-deepgray;
+    color: $color-white;
+    display: flex;
+    justify-content: center;
+    ul {
+      display: flex;
+      justify-content: space-around;
+      width: 50%;
+      line-height: 50px;
+      li {
+        cursor: pointer;
+        &.avatar img {
+          width: 70px;
+          height: 70px;
+          border-radius: 50%;
+        }
+      }
     }
   }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home">
     <top-header></top-header>
+    <div class="blog-list">
+      <div v-for="(item, i) in data" :key="i">{{ item }}</div>
+      <div v-for="(item, i) in data" :key="i">{{ item }}</div>
+      <div v-for="(item, i) in data" :key="i">{{ item }}</div>
+    </div>
   </div>
 </template>
 
@@ -8,8 +13,25 @@
   import TopHeader from '@/components/front/TopHeader'
   export default {
     name: 'home',
+    data() {
+      return {
+        data: [1,2,3,4,5,6,7,8,9,10,11,12,13,15]
+      }
+    },
     components: {
       TopHeader
     }
   }
 </script>
+
+<style lang="scss" scoped>
+  @import '@/common/css/variable.scss';
+  .home {
+    color: $color-black;
+  }
+  .blog-list {
+    background: $color-white;
+    margin-top: 520px;
+  }
+</style>
+
