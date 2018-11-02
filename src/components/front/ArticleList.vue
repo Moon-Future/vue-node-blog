ccecb2b6ea10f42b8049887e5d2e152d1de01592
Leo Chan <236338364@qq.com>
2018-11-03 01:41:05 +0800

    home article

--- src/components/front/ArticleList.vue
+++ src/components/front/ArticleList.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="article-list">
     <div class="article-wrapper" v-for="(article, i) in articleList" :key="i">
-      <div class="txt">
+      <div class="text">
         <div class="title">{{ article.title }}</div>
+        <div class="message">
+          <div class="msg time">{{ article.createTime }}</div>
+          <div class="msg view">
+            <icon-font icon="icon-view"></icon-font>{{ article.view }}
+          </div>
+          <div class="msg like">
+            <icon-font icon="icon-like"></icon-font>{{ article.view }}
+          </div>
+        </div>
         <div class="markdown-body" v-html="article.summary"></div>
       </div>
-      <div class="pic">
-        <img v-lazy="`https://source.unsplash.com/200x200/weekly?nature,${Math.random()}`" alt="pic">
+      <div class="picture">
+        <img v-lazy="`https://source.unsplash.com/200x200/weekly?nature,${i}`" alt="pic">
       </div>
     </div>
   </div>
@@ -15,6 +24,7 @@
 <script>
   import IconFont from '@/components/Iconfont'
   import apiUrl from '@/serviceAPI.config.js'
+  import { dateFormat } from '@/common/js/tool.js'
   export default {
     name: 'articleList',
     data() {
@@ -32,11 +42,17 @@
         }).then(res => {
           if (res.data.code === 200) {
             this.articleList = res.data.message
+            this.articleList.forEach(ele => {
+              ele.createTime = dateFormat(ele.createTime, 'yyyy-MM-dd')
+            })
           } else {
             this.$message.error(res.data.message)
           }
         })
       }
+    },
+    components: {
+      IconFont
     }
   }
 </script>
@@ -50,13 +66,21 @@
       box-shadow: 0px 0px 5px $color-gray;
       padding: 10px;
       margin-bottom: 20px;
-      .txt {
+      .text {
         text-align: left;
         margin-right: 10px;
         .title {
           font-weight: bold;
           font-size: 22px;
-          margin-bottom: 10px;
+          margin-bottom: 5px;
+        }
+        .message {
+          display: flex;
+          color: $color-gray;
+          font-size: 14px;
+          .msg {
+            margin-right: 10px;
+          }
         }
       }
     }
