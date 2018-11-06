f14f628b627a8f18ea097058ecca3c551b906ab5
LeoChan <236338364@qq.com>
2018-11-06 21:56:30 +0800

    catalog

--- src/views/front/Catalog.vue
+++ src/views/front/Catalog.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="catalog-wrapper">
-    <div class="month-devide" v-for="(item, i) in catalog" :key="i">
-      <h1>{{ item.title }}</h1>
+    <div class="month-devide" v-for="(list, key) in catalog" :key="key">
+      <h1 class="month">{{ key }}</h1>
+      <div class="month-data" v-for="(item, i) in list" :key="i">
+        <span class="content day">{{ item.day }}</span>
+        <span class="content title">{{ item.title }}</span>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
   import apiUrl from '@/serviceAPI.config.js'
+  import { dateFormat } from '@/common/js/tool.js'
   export default {
+    name: 'catalog',
     data() {
       return {
-        catalog: []
+        catalog: {}
       }
     },
     created() {
@@ -23,7 +29,16 @@
           data: {catalog: true}
         }).then((res) => {
           if (res.data.code === 200) {
-            this.catalog = res.data.message
+            let data = res.data.message
+            data.forEach(ele => {
+              let time = dateFormat(ele.createTime, 'yyyy-MM')
+              let day = dateFormat(ele.createTime, 'yyyy-MM-dd').split('-')[2]
+              ele.day = day
+              if (!this.catalog[time]) {
+                this.$set(this.catalog, time, [])
+              }
+              this.catalog[time].push(ele)
+            })
           } else {
             this.$message.error(res.data.message)
           }
@@ -37,8 +52,36 @@
   @import '@/common/css/variable.scss';
   .catalog-wrapper {
     text-align: left;
-    .month-devide {
-      
+  }
+  .month-devide {
+    .month {
+      font-weight: bold;
+      font-size: 22px;
+      margin-bottom: 10px;
+    }
+    .month-data {
+      border-left: 1px solid $color-gray;
+      border-right: 1px solid $color-gray;
+      border-bottom: 1px solid $color-gray;
+      display: flex;
+      &:nth-child(1) {
+        background: $color-red;
+
+      }
+    }
+  }
+  .content {
+    padding: 10px;
+    &:hover {
+      background: $color-gray;
+    }
+    &.day {
+      border-right: 1px solid $color-gray;
+    }
+    &.title {
+      cursor: pointer;
+      width: 100%;
+      display: inline-block;
     }
   }
 </style>
