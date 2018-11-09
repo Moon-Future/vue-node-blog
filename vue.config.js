0b2feb9b7941baa6c116a2a33291bf6da720cbd9
Leo Chan <236338364@qq.com>
2018-11-09 21:09:05 +0800

    port

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000/api',
+        target: 'http://localhost:3001/api',
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
